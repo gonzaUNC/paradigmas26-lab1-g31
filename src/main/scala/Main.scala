f2746483fd2f1298949a30ce5c2e4a9283853c32
mateohered1a <mateoheredia08@mi.unc.edu.ar>
2026-04-05 23:42:59 -0300

    feat(Main):calculamos frecuencia de palabras por subreddit usando WordCount.counting

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,7 @@ import javax.swing.text.Position
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(s"Reddit Post Parser\n${"=" * 40}")
+    println(s"Reddit Post Parser\n${"=" * 40}") //deco
 
     // leemos las suscripciones del archivo json
     val subscriptions = FileIO.readSubscriptions("subscriptions.json").getOrElse(List.empty)
@@ -15,13 +15,22 @@ object Main {
     }
 
     // realizamos el filtro de post no validos
-      val validPosts: List[Post] = FileIO.filterPosts(allPosts)
+    val validPosts: List[Post] = FileIO.filterPosts(allPosts)
 
     // agrupamos por subreddit para mostrar cada uno por separado
     val bySubreddit = validPosts.groupBy { case (subreddit, _, _, _) => subreddit }
 
+    // contamos frecuencia de palabras por subreddit
+    val wordsBySubreddit = bySubreddit.map { case (name, posts) =>
+      (name, WordCount.counting(posts))
+    }
+
     val output = subscriptions
-      .map { case (name, _) => Formatters.formatSubscription(name, bySubreddit.getOrElse(name, List.empty)) }
+      .map { case (name, _) =>
+        val posts = bySubreddit.getOrElse(name, List.empty)
+        val words = wordsBySubreddit.getOrElse(name, Map.empty)
+        Formatters.formatSubscription(name, posts, words)
+      }
       .mkString("\n")
 
     println(output)
