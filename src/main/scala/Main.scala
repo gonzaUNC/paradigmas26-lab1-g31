66537046079dfdbb17eddbaaf3ba5e2ba16b5820
Gonzalo Fonseca <gonzafonseca@mi.unc.edu.ar>
2026-04-06 19:53:48 -0300

    feat(Main): se agrega un parametro más en los case para que no de error al compilar

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -18,7 +18,7 @@ object Main {
     val validPosts: List[Post] = FileIO.filterPosts(allPosts)
 
     // agrupamos por subreddit para mostrar cada uno por separado
-    val bySubreddit = validPosts.groupBy { case (subreddit, _, _, _) => subreddit }
+    val bySubreddit = validPosts.groupBy { case (subreddit, _, _, _, _) => subreddit }
 
     // contamos frecuencia de palabras por subreddit
     val wordsBySubreddit = bySubreddit.map { case (name, posts) =>
