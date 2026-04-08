c4b6d57fda4e8fd3f916770f1f81b45397ae5336
cotibodereau <constanzabodereau@mi.unc.edu.ar>
2026-04-08 11:32:44 -0300

    feat(FileIO): se agrega campo url

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -57,7 +57,7 @@ object FileIO {
         val date       = TextProcessing.formatDateFromUTC(createdUtc)
         val score      = (data \ "score").extract[Int]
         val url        = (data \ "url").extract[String]
-        (subreddit, title, selftext, date, score)
+        (subreddit, title, selftext, date, score, url)
       }
       Some(posts)
     } catch {
