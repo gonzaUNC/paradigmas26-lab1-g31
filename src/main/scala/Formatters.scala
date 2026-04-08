9dbff15458541ff477876f2f1822f61436a86a86
mateohered1a <mateoheredia08@mi.unc.edu.ar>
2026-04-08 18:33:39 -0300

    feat(Formatters): agregamos url en la salida de funcion formatpost

--- src/main/scala/Formatters.scala
+++ src/main/scala/Formatters.scala
@@ -3,14 +3,14 @@ import FileIO.Post
 object Formatters {
 
   def formatPost(post: Post): String = {
-    val (subreddit, title, selftext, date, score) = post
+    val (subreddit, title, selftext, date, score, url) = post
     // mostramos un preview del texto para no llenar la pantalla
     val preview = if (selftext.length > 200) selftext.take(200) + "..." else selftext
-    s"[$subreddit] $title\nFecha: $date\nScore: $score\n$preview"
+    s"[$subreddit] $title\nFecha: $date\nScore: $score\nURL: $url\n$preview"
   }
 
-  def formatSubscription(subreddit: String, posts: List[Post], words: Map[String, Int]): String = {
-    val header = s"\n${"=" * 80}\nSubreddit: $subreddit\n${"=" * 80}"
+  def formatSubscription(subreddit: String, posts: List[Post], words: Map[String, Int], scoreTotal: Int): String = {
+    val header = s"\n${"=" * 80}\nSubreddit: $subreddit |Score Total: $scoreTotal\n${"=" * 80}"
     val body   = posts.map(formatPost).mkString("\n" + "-" * 40 + "\n")
 
     // palabras mas frecuentes ordenadas de mayor a menor
