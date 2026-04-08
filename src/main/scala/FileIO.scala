5d2136aeca4051a0dd3ec2902a74b286db006aa6
mateohered1a <mateoheredia08@mi.unc.edu.ar>
2026-04-08 12:30:34 -0300

    feat(fix): fix de estructura

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -57,7 +57,7 @@ object FileIO {
         val date       = TextProcessing.formatDateFromUTC(createdUtc)
         val score      = (data \ "score").extract[Int]
         val url        = (data \ "url").extract[String]
-        (subreddit, title, selftext, date, score, url)
+        Post(subreddit, title, selftext, date, score, url)
       }
       Some(posts)
     } catch {
